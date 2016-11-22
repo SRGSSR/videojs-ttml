fa62298f8d93ae9c663d19beaa1551623ced7b35
Josep Boix Requesens <jspboix@gmail.com>
2016-11-22 13:41:17 +0100

    Added background color and color as cue properties

--- dist/videojs-ttml.js
+++ dist/videojs-ttml.js
@@ -1,4 +1,4 @@
-/*! videojs-ttml - v0.0.1 - 2016-11-21*/
+/*! videojs-ttml - v0.0.1 - 2016-11-22*/
 (function(window, moment, vjs) {
   'use strict';
 
@@ -7,34 +7,35 @@
   }
 
   TTMLParser.prototype = {
-    parseTTMLText_: function(p) {
+    parseTTMLCues_: function(p) {
       var lines = p.getElementsByTagName('span'),
-          txt = "";
+          startTime = moment.duration(p.getAttribute('begin')).asSeconds(),
+          endTime = moment.duration(p.getAttribute('end')).asSeconds(),
+          id = p.getAttribute('xml:id'),
+          cues = [];
 
       for (var i = 0; i < lines.length; i++) {
-        var l = lines[i], txtContent = l.textContent.trim(),
-            color = l.getAttribute('tts:color'),
-            bColor = l.getAttribute('tts:backgroundColor');
+        var l = lines[i];
 
-        txt += '<span style="background-color:' + bColor +
-               ';color:' + color + '">' + txtContent + '</span>';
+        cues.push({
+          startTime: startTime,
+          endTime: endTime,
+          id: id + '_' + i,
+          text: l.textContent.trim(),
+          color: l.getAttribute('tts:color'),
+          backgroundColor: l.getAttribute('tts:backgroundColor')
+        });
       }
 
-      return txt;
+      return cues;
     },
 
-    parseTTMLCues: function(ttml) {
+    parseTTML: function(ttml) {
       var dom = this.domParser_.parseFromString(ttml, 'text/html'),
           pS = dom.getElementsByTagName('p'), cues = [];
 
       for (var i = 0; i < pS.length; i++) {
-        var p = pS[i];
-        cues.push({
-          startTime: moment.duration(p.getAttribute('begin')).asSeconds(),
-          endTime: moment.duration(p.getAttribute('end')).asSeconds(),
-          id: p.getAttribute('xml:id'),
-          text: this.parseTTMLText_(p)
-        });
+        cues = cues.concat(this.parseTTMLCues_(pS[i]));
       }
 
       return cues;
@@ -59,16 +60,16 @@
           var track = elmt.track;
           track.loaded_ = false;
           vjs.xhr(src, {}, function (err, response, responseBody) {
-            if (err) {
-              track.loaded_ = false;
+            if (err || response.statusCode >= 300) {
+              this.removeRemoteTextTrack(track);
             } else {
               track.loaded_ = true;
-              var cues = parser.parseTTMLCues(responseBody);
+              var cues = parser.parseTTML(responseBody);
               cues.forEach(function(cue) {
                 elmt.track.addCue(cue);
               });
             }
-          });
+          }.bind(this));
         }
 
         return elmt;
